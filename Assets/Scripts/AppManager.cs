ff503c9b74396aefc0f2423043ff989872ea20bf
parthgp2 <parthgp2@illinois.edu>
2018-05-31 18:02:00 -0500

    More or less done with UI. Have to figure out how sound will work and make things actaully look nice, but that can be done later.

---
+++ Assets/Scripts/AppManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AppManager : MonoBehaviour {
+    /*
+    public void SetSoundMuteImage()
+    {
+        GameObject toggle = GameObject.FindGameObjectsWithTag("SoundToggle") as GameObject;
+    }
+    */
+    public void LoadSceneByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void QuitOnClick()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.QUIT();
+#endif
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
