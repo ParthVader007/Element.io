9ce533b70dd4ee20bc97dbbf170f220452bf5936
parthgp2 <parthgp2@illinois.edu>
2018-05-31 17:59:15 -0500

    More or less done with UI, have a few things to fix up.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    public bool fire;
+    public bool water;
+    public bool earth;
+    public GameObject[] elements;
 
+    public void ChangeToFire()
+    {
+        fire = true;
+        water = false;
+        earth = false;
+    }
+
+    public void ChangeToWater()
+    {
+        fire = false;
+        water = true;
+        earth = false;
+    }
+
+    public void ChangeToEarth()
+    {
+        fire = false;
+        water = false;
+        earth = true;
+    }
 	// Use this for initialization
 	void Start () {
 		
